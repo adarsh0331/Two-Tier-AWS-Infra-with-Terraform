daca32d17016a2e20bfa1c829c68e1668bbc00a2
Barkunta Adarsh <129056162+adarsh0331@users.noreply.github.com>
2025-12-19 20:38:26 +0530

    Update S3 key path for Terraform backend

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-ews-baket1"
     region         = "us-east-1"
-    key            = "DevOps Project-11/terraform.tfstate"
+    key            = "DevOps Project/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
